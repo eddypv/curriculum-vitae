f9d5aa6a4efe4a767ca3bf61223a384206edb1f1
eddypv <eddypv0@gmail.com>
2018-11-06 17:11:14 -0400

    [INIT] Commit inicial

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import renderDOM from 'react-dom'
+import Home from './js/containers/home.jsx'
+const app = document.getElementById('app')
+renderDOM.render(<Home />,app)
---
+++ src/js/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header(props){
+    return (
+        <div>
+            <img src="" alt=""/>
+        </div>
+    )
+}
+
+export default Header
