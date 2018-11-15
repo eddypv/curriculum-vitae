bec7e8517579b0f935f90c42363c707bd686372f
eddypv <eddypv0@gmail.com>
2018-11-15 17:02:26 -0400

    [VERSION] Version 1.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,81 @@
 const path = require('path')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CleanWebPackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack')
-module.exports ={
-    entry:"./src/index.js",
-    output:{
-        path:path.resolve(__dirname, 'dist'),
-        filename:'bundle.js',
-        publicPath:"http://localhost:8080/dist/" 
-    },
-    mode:'development',
-    module:{
-        rules:[
-            {
-                test:/\.(js|jsx)$/,
-                exclude:/(node_modules)/,
-                use:{
-                    loader:'babel-loader',
-                    options:{
-                        presets:['es2015', 'react', 'stage-2']
-                    }
-                }
-            },
-            {
-                test:/\.(png|jpg|gif|svg)$/,
-                use:{
-                    loader:'url-loader',
-                    options:{
-                        fallback:'file-loader',
-                        limit:1000,
-                        name:'images/[name].[ext]'
+const plugins =[
+    new miniCssExtractPlugin({
+        filename: "./css/[name].css",
+        chunkFilename: "[id].css"
+    }),
+    new webpack.DllReferencePlugin({
+        manifest:"./vendor-manifest.json"
+    })
+]
 
-                    }
+module.exports =(env)=>{
+    
+    plugins.push(
+        new CleanWebPackPlugin(['dist'], {root:__dirname})
+    )
+    
+    return {
+        entry:{
+            bundle:path.resolve(__dirname, "src/index.js")
+        },
+        output:{
+            path:path.resolve(__dirname, "dist"),
+            filename :'js/[name].js',
+            publicPath:"http://localhost:8080/dist/", 
+            chunkFilename:'js/[id].js'
+
+        },
+        optimization: {
+            minimizer: [
+                new UglifyJsPlugin(),
+                new OptimizeCSSAssetsPlugin({})
+            ]
+        },
+        mode:'production',
+        devServer:{
+            port:9000
+        },
+        module: {
+            rules: [
+            {
+                // test: que tipo de archivo quiero reconocer,
+                // use: que loader se va a encargar del archivo
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015', 'react', 'stage-2'],
                 }
+                },
             },
             {
                 test:/\.css$/,
-                use:['style-loader', 'css-loader']
-            }
-        ]
-    },
-    plugins:[
-        new webpack.DllReferencePlugin({
-            manifest:"./vendor-manifest.json"
-        })
-    ]
+                use:[
+                    miniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 1000,
+                    fallback: 'file-loader',
+                    name: 'images/[name].[ext]'
+                }
+                }
+            },
+
+            ]
+        },
+        plugins
+    }
 }
---
+++ webpack.dev.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const webpack = require('webpack')
+module.exports ={
+    entry:"./src/index.js",
+    output:{
+        path:path.resolve(__dirname, 'dist'),
+        filename:'bundle.js',
+        publicPath:"http://localhost:8080/dist/" 
+    },
+    mode:'development',
+    module:{
+        rules:[
+            {
+                test:/\.(js|jsx)$/,
+                exclude:/(node_modules)/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['es2015', 'react', 'stage-2']
+                    }
+                }
+            },
+            {
+                test:/\.(png|jpg|gif|svg)$/,
+                use:{
+                    loader:'url-loader',
+                    options:{
+                        fallback:'file-loader',
+                        limit:1000,
+                        name:'images/[name].[ext]'
+
+                    }
+                }
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins:[
+        new webpack.DllReferencePlugin({
+            manifest:"./vendor-manifest.json"
+        })
+    ]
+}
